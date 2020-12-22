5ec3fe194f2013042c87e73d4bb8105e60069ecf
soflybird <74286728+soflybird@users.noreply.github.com>
2020-12-22 10:52:01 -0600

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,31 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"]
-        }
-      ]
-    },
-    //
-  };
+  entry: { index: path.resolve(__dirname, "src", "index.js") },
+  output: {
+    path: path.resolve(__dirname, "build"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src", "index.html"),
     }),
-  ]
- 
+  ],
+};
+
+
